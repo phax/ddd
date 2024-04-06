b6acdcd971dd22045206f0250c21160f31ada75e
Philip Helger <philip@helger.com>
2024-04-06 18:11:32 +0200

    Added equals/hashCode

--- src/main/java/com/helger/ddd/model/VPSourceValue.java
+++ src/main/java/com/helger/ddd/model/VPSourceValue.java
@@ -19,6 +19,9 @@ package com.helger.ddd.model;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.hashcode.HashCodeGenerator;
 import com.helger.commons.string.ToStringGenerator;
 
 /**
@@ -33,20 +36,43 @@ public final class VPSourceValue
   private final EDDDSourceField m_eSourceField;
   private final String m_sSourceValue;
 
-  public VPSourceValue (@Nonnull final EDDDSourceField eSourceField, @Nonnull final String sSourceValue)
+  public VPSourceValue (@Nonnull final EDDDSourceField eSourceField, @Nonnull @Nonempty final String sSourceValue)
   {
+    ValueEnforcer.notNull (eSourceField, "SourceField");
+    ValueEnforcer.notEmpty (sSourceValue, "SourceValue");
+
     m_eSourceField = eSourceField;
     m_sSourceValue = sSourceValue;
   }
 
+  @Nonnull
   public EDDDSourceField getSourceField ()
   {
     return m_eSourceField;
   }
 
+  @Nonnull
+  @Nonempty
   public String getSourceValue ()
   {
     return m_sSourceValue;
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (o == null || !getClass ().equals (o.getClass ()))
+      return false;
+    final VPSourceValue rhs = (VPSourceValue) o;
+    return m_eSourceField.equals (rhs.m_eSourceField) && m_sSourceValue.equals (rhs.m_sSourceValue);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_eSourceField).append (m_sSourceValue).getHashCode ();
   }
 
   @Override
